6df03b6f90a83b3604e44224598c009d55a39934
D044217 <773360120@qq.com>
2020-03-15 15:31:14 +0800

    结构搭建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+//导入路由 vue-router
+import router from "./router/index";
+//导入element_ui
+import ElementUI from 'element-ui';
+//导入element_ui 样式
+import 'element-ui/lib/theme-chalk/index.css';
+//注册element_ui
+Vue.use(ElementUI);
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  router
+}).$mount("#app");
---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+//导入vue-router
+import vueRouter from "vue-router";
+//导入vue
+import Vue from "vue";
+//注册vue-router
+Vue.use(vueRouter);
+//配置router
+var router = new vueRouter({
+    routes:[
+        
+    ]
+});
+export default router
